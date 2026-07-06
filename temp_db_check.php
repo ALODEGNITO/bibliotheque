28ee6849b86a85f6d7a414e8fe6a2546b17c35c5
ALODEGNITO Tolès Tanguy <132446493+ALODEGNITO@users.noreply.github.com>
2026-07-06 09:45:27 +0100

    Delete temp_db_check.php

--- temp_db_check.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-$config = require 'c:/laragon-new/www/bibliotheque/app/config.php';
-$db = $config['db'];
-$dsn = 'mysql:host=' . $db['host'] . ';dbname=' . $db['name'];
-$pdo = new PDO($dsn, $db['user'], $db['pass']);
-foreach ($pdo->query('DESCRIBE livres') as $row) {
-    echo $row['Field'] . "\t" . $row['Type'] . "\n";
-}
